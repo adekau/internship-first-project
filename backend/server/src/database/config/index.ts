2fd92d88f617ba8049956c8527642f550b7e819d
Alex Dekau <alex.potatofarms@gmail.com>
2018-05-18 15:02:19 -0400

    Add comments to remind me that some files are useless

--- backend/server/src/database/config/index.ts
+++ backend/server/src/database/config/index.ts
@@ -1,5 +1,8 @@
 // file to configure and create sequelize instance
 
+// FOR THE MOMENT, THIS FILE IS USELESS. 
+// Database connection is handled in 'models/index.ts'
+
 import * as Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('incidents', 'Alex', '', {
--- backend/server/src/database/index.ts
+++ backend/server/src/database/index.ts
@@ -1,4 +1,8 @@
 // function that will set associations between models and sync the database
+
+// FOR THE MOMENT THIS FILE IS USELESS.
+// Database associations are handled in the individual model files ('/models/*.ts').
+
 import { db } from './config';
 import { models } from '../models';
 
