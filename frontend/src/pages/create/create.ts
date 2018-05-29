0e7d2b6169a8df3fc8f1d6183d22d93b4b974144
Alex Dekau <alex.potatofarms@gmail.com>
2018-05-29 13:25:52 -0400

    Create form for creating an incident

--- frontend/src/pages/create/create.ts
+++ frontend/src/pages/create/create.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
+import * as _ from 'lodash';
+import { RestProvider } from '../../providers/rest/rest';
 
 /**
  * Generated class for the CreatePage page.
@@ -10,16 +12,85 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-create',
-  templateUrl: 'create.html',
+    selector: 'page-create',
+    templateUrl: 'create.html',
 })
+
 export class CreatePage {
+    private allowedToLeave: boolean = false;
+    private user: any = {};
+    private trackers: any;
+    private incident: any = {
+        resolution: ""
+    };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private alertCtrl: AlertController,
+        private rest: RestProvider,
+        private toast: ToastController
+    ) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreatePage');
-  }
+    ionViewDidLoad() {
+        this.getTrackers();
+        this.getUser();
+    }
 
+    private getUser() {
+        this.user = JSON.parse(localStorage.getItem('logged-in-user'));
+    }
+
+    private getTrackers() {
+        let token = localStorage.getItem('access-token');
+        this.rest.getTrackers(token)
+            .subscribe(trackers => this.trackers = trackers);
+    }
+
+    private formHasChanged(): boolean {
+        return !_.isEqual(this.incident, { resolution: "" });
+    }
+
+    ionViewCanLeave() {
+        if (this.formHasChanged() && !this.allowedToLeave) {
+            let alertPopup = this.alertCtrl.create({
+                title: 'Discard unsaved form data?',
+                message: 'Inputted information will not save if you leave. Are you sure?',
+                buttons: [{
+                    text: 'Exit',
+                    handler: () => {
+                        this.allowedToLeave = true;
+                        this.navCtrl.pop();
+                    }
+                },
+                {
+                    text: 'Stay',
+                    handler: () => {
+                        // Stuff to do if stay is pressed.
+                    }
+                }]
+            });
+
+            alertPopup.present();
+
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    createIncident() {
+        this.incident.userId = this.user.id;
+        let token = localStorage.getItem('access-token');
+        this.rest.createIncident(token, this.incident)
+            .then((res: any) => {
+                this.toast.create({
+                    message: `${res.text}`,
+                    duration: 3000,
+                    position: 'top'
+                }).present();
+                this.allowedToLeave = true;
+                this.navCtrl.pop();
+            });
+    }
 }
